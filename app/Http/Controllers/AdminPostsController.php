cb92146e6dd4d39574d830b8705a7cda62bd134a
Lorenz Natividad <lorenznatividad1996@gmail.com>
2017-09-20 11:49:37 +0800

    admin posts create - no dynamic category

--- app/Http/Controllers/AdminPostsController.php
+++ app/Http/Controllers/AdminPostsController.php
@@ -2,6 +2,12 @@
 
 namespace App\Http\Controllers;
 
+
+use Illuminate\Support\Facades\Session;
+use App\Photo;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Requests\PostsCreateRequest;
+use App\Post;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
@@ -16,6 +22,10 @@ class AdminPostsController extends Controller
     public function index()
     {
         //
+        $posts = Post::all();
+
+        return view('admin.posts.index', compact('posts'));
+
     }
 
     /**
@@ -26,6 +36,8 @@ class AdminPostsController extends Controller
     public function create()
     {
         //
+
+        return view('admin.posts.create');
     }
 
     /**
@@ -34,9 +46,35 @@ class AdminPostsController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(PostsCreateRequest $request)
     {
         //
+        // return $request->all();
+
+        $input = $request->all();
+
+        $user = Auth::user();
+
+        if($file = $request->file('photo_id')) {
+
+            $name = time() . $file->getClientOriginalName();
+
+            $file->move('images', $name);
+
+            $photo = Photo::create(['file'=>$name]);
+
+            $input['photo_id'] = $photo->id;
+
+        }
+
+        $user->posts()->create($input);
+
+         Session::flash('added_post', 'The post has been added');
+
+        return redirect('/admin/posts');
+
+
+
     }
 
     /**
---
+++ resources/views/admin/posts/index.blade.php
@@ -0,0 +1,53 @@
+@extends('layouts.admin')
+
+
+
+@section('content')
+
+	@if(Session::has('added_post'))
+		<div class="alert alert-success">
+		  <strong>{{session('added_post')}}!</strong> 
+		</div>
+	@endif
+
+	<h1>Posts</h1>
+
+	<table class="table">
+		<thead>
+			<tr>
+				<th>Id</th>
+				<th>Photo</th>
+				<th>User</th>
+				<th>Category</th>
+				<th>Title</th>
+				<th>Body</th>
+				<th>Created</th>
+				<th>Updated</th>
+			</tr>	
+		</thead>
+
+		<tbody>
+		
+		@if($posts)
+
+			@foreach($posts as $post)
+
+			<tr>
+				<td>{{$post->id}}</td>
+				<td><img src="{!!asset($post->photo ? $post->photo->file : 'http://placehold.it/400x400' )!!}" height="50" alt=""></td>
+				<td>{{$post->user->name}}</td>
+				<td>{{$post->category_id}}</td>
+				<td>{{$post->title}}</td>
+				<td>{{$post->body}}</td>
+				<td>{{$post->created_at->diffForHumans()}}</td>
+				<td>{{$post->updated_at->diffForHumans()}}</td>
+			</tr>
+		
+			@endforeach
+
+		@endif
+
+		</tbody>
+	</table>
+
+@stop
